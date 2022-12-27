b28e5af8b2951b9bb16bfddcf819e8abaa6adc53
Sarah <sarah.celiann@gmail.com>
2022-12-27 17:24:05 -0600

    started adding wincon check

--- app.js
+++ app.js
@@ -9,13 +9,15 @@ const playerOneSection = document.getElementById("player-one");
 const playerTwoSection = document.getElementById("player-two");
 const currentPlayerDisplay = document.getElementById("current-player");
 const versus = document.getElementsByTagName("h2");
+const testButton = document.getElementById("test");
 
 //Game State
 const gameState = {
     currentPlayer: "Player 1",
     playerOneName: "",
     playerTwoName: "",
-    playersSelected: false
+    playersSelected: false,
+    boardState: [["", "", ""], ["", "", ""], ["", "", ""]]
 }
 
 //Generate blank board
@@ -29,8 +31,8 @@ const makeBoard = () => {
 }
 
 //Initial Run
-makeBoard();
-const cells = document.getElementsByClassName("cell");
+// makeBoard();
+// const cells = document.getElementsByClassName("cell");
 
 //clicking adds an x to the board
 
@@ -68,7 +70,63 @@ playersSection.addEventListener("click", (event) => {
         versus[0].style.display = "block";
         gameState.currentPlayer = gameState.playerOneName;
         currentPlayerDisplay.innerText = `Current Player: ${gameState.currentPlayer}`;
+        currentPlayerDisplay.style.display = "flex";
         gameState.playersSelected = true;
+        makeBoard();
+        const cells = document.getElementsByClassName("cell");
     }
 });
 
+// Check for victory
+//get the content of the board
+const getBoardState = (array) => {
+    let count = 0;
+    console.log (array[count].innerHTML);
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+            const cellStatus = array[count].innerHTML;
+            if(cellStatus === '<i class="fa-solid fa-x"></i>') {
+                gameState.boardState[i][j] = "X";
+                count++;
+            }
+            else if(cellStatus === '<i class="fa-solid fa-o"></i>') {
+                gameState.boardState[i][j] = "O";
+                count++;
+            } else {
+                count++;
+            }
+        }
+    }
+};
+//get one row of the board
+const getRow = (array, index) => {
+    let row = [];
+    for (let i = 0; i < array[index].length; i++) {
+        row.push(array[index][i]);
+    }
+    return row;
+}
+//get one column of the board
+const getCol = (array, index) => {
+    let col = [];
+    for (let i = 0; i < array.length; i++) {
+        col.push(array[i][index]);
+    }
+    return col;
+}
+//check for 3 in a row vertically
+
+//check for 3 in a row horizontally
+//check for 3 in a row diagonally
+
+//Test button
+testButton.addEventListener("click", () => {
+    const board = getBoardState(document.getElementsByClassName("cell"));
+    // console.log(document.getElementsByClassName("cell"));
+    console.log(gameState.boardState);
+    console.log(getRow(gameState.boardState, 0));
+    console.log(getCol(gameState.boardState, 1));
+});
+
+//Bugs
+//entering same name means players don't switch
