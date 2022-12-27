4ebf3bb1e72d1536f82fa16ce2fd5c21a0c27e0d
Sarah <sarah.celiann@gmail.com>
2022-12-26 18:19:09 -0600

    switching players works

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 //DOM Selectors
 const board = document.getElementById("board");
+const playerOneInput = document.getElementById("player-one-input");
+const playerTwoInput = document.getElementById("player-two-input");
+const playerOneButton = document.getElementById("player-one-button");
+const playerTwoButton = document.getElementById("player-two-button");
+const playersSection = document.getElementById("players");
+const playerOneSection = document.getElementById("player-one");
+const playerTwoSection = document.getElementById("player-two");
+const currentPlayerDisplay = document.getElementById("current-player");
+const versus = document.getElementsByTagName("h2");
 
 //Game State
 const gameState = {
-    currentPlayer: "Player 1"
+    currentPlayer: "Player 1",
+    playerOneName: "",
+    playerTwoName: ""
 }
 
 //Generate blank board
@@ -24,16 +35,36 @@ const cells = document.getElementsByClassName("cell");
 
 board.addEventListener("click", (event) => {
    if (event.target.tagName === "DIV"){ 
-    if(gameState.currentPlayer === "Player 1") {
+    if(gameState.currentPlayer === gameState.playerOneName) {
         const targetCell = event.target;
         targetCell.innerHTML = '<i class="fa-solid fa-x"></i>';
-        gameState.currentPlayer = "Player 2";
+        gameState.currentPlayer = gameState.playerTwoName;
+        currentPlayerDisplay.innerText = `Current Player: ${gameState.currentPlayer}`;
     } else {
         const targetCell = event.target;
         targetCell.innerHTML = '<i class="fa-solid fa-o"></i>';
-        gameState.currentPlayer = "Player 1";
+        gameState.currentPlayer = gameState.playerOneName;
+        currentPlayerDisplay.innerText = `Current Player: ${gameState.currentPlayer}`;
     }
    }
    console.log(gameState.currentPlayer);
+});
+
+//Click to set player names
+playersSection.addEventListener("click", (event) => {
+    const buttonId = event.target.getAttribute("id");
+    if(buttonId === "player-one-button") {
+        gameState.playerOneName = playerOneInput.value;
+        playerOneInput.value = "";
+        playerOneSection.style.display = "none";
+    } else if (buttonId === "player-two-button") {
+        gameState.playerTwoName = playerTwoInput.value;
+        playerTwoInput.value = "";
+        playerTwoSection.style.display = "none";
+    }
+    if (gameState.playerOneName !== "" && gameState.playerTwoName !== "") {
+        versus[0].innerText = `It's ${gameState.playerOneName} vs ${gameState.playerTwoName}`;
+        versus[0].style.display = "block";
+    }
 });
 
